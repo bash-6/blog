dbcb4d8bbc5ba085a1463a541dd8772963244d6c
angel <sandex7@protonmail.com>
2022-06-15 11:05:44 -0500

    definiendo responsive

--- js/main.js
+++ js/main.js
@@ -40,17 +40,35 @@ $('#close-modal').addEventListener('click',()=>{
 })
 
 //cambiar tema a dark
-$('#theme').addEventListener('click',()=>{
-	if ($('.acerca').classList.contains('light')) {
-		$('.acerca').style.backgroundColor = '#222'
-		$('.acerca').style.color = '#fff'
-		$('.acerca').classList.remove('light')
-		$('.acerca').classList.add('dark')
-		
-	} else if($('.acerca').classList.contains('dark')){
-		$('.acerca').style.backgroundColor = '#fff'
-		$('.acerca').style.color = '#222'
-		$('.acerca').classList.remove('dark')
-		$('.acerca').classList.add('light')
-	}
+$$('.theme').forEach(e => {
+	e.addEventListener('click',()=>{
+		if ($('.acerca').classList.contains('light')) {
+			$('.acerca').style.backgroundColor = '#222'
+			$('.acerca').style.color = '#fff'
+			$('.acerca').classList.remove('light')
+			$('.acerca').classList.add('dark')
+			$('.acerca .fa-sun').style.display = 'block'
+			$('.acerca .fa-moon').style.display = 'none'
+			
+		} else if($('.acerca').classList.contains('dark')){
+			$('.acerca').style.backgroundColor = '#fff'
+			$('.acerca').style.color = '#222'
+			$('.acerca').classList.remove('dark')
+			$('.acerca').classList.add('light')
+			$('.acerca .fa-sun').style.display = 'none'
+			$('.acerca .fa-moon').style.display = 'block'
+		}
+	})
+})
+
+//scroll animado
+let card = $$('.card-herramienta')
+window.addEventListener('scroll',()=>{
+	let scrollTop = document.documentElement.scrollTop
+	card.forEach(e => {
+		let alturaAnimado = e.offsetTop
+		if (alturaAnimado -550 < scrollTop) {
+			e.style.opacity = '1'
+		}
+	})
 })
